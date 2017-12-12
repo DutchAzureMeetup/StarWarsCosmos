13d11e11f5711195cfd54286415b10a7ebeb278c
Pascal Naber <pascal.naber@gmail.com>
2017-12-12 22:04:52 +0100

    added console app to read json and fill table storage

---
+++ src/ImportKeyValueDatabase/ConsoleApp1/Planetes.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure.CosmosDB.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportKeyValueDatabase
+{
+    public class Planet : TableEntity
+    {
+        public Planet()
+        {
+
+        }
+        public Planet(string planetName, string coordinate)
+        {
+            this.PartitionKey = "planet";
+            this.RowKey = planetName;
+        }
+
+        public string Coordinate { get; set; }
+    }
+}
---
+++ src/ImportKeyValueDatabase/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using ImportKeyValueDatabase;
+using Microsoft.Azure;
+using Microsoft.Azure.CosmosDB.Table;
+using Microsoft.Azure.Storage;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetes = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("planets.json"));
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("tableStorage"));
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table and delete it.
+            CloudTable table = tableClient.GetTableReference("planets");
+            table.DeleteIfExists();
+
+            // Create the table if it doesn't exist.
+            table.Create();
+
+            foreach (var item in planetes)
+            {
+                table.Execute(TableOperation.Insert(new Planet(item.Key, item.Value)));
+            }
+        }
+    }
+}
